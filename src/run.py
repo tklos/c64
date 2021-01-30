10c0b2bf6d8e8e24a8d331647d1508f35800f7e9
tklos <tomasz.klos9@gmail.com>
2021-01-30 20:37:21 +0100

    Add video preview

--- src/run.py
+++ src/run.py
@@ -20,7 +20,7 @@ SERIAL_BAUDRATE = 19200
 _CMD_QUEUE_SENTINEL = object()
 
 
-# Replay video settings
+# Video settings
 VID_DEVICE_ID = 2
 VID_WIDTH, VID_HEIGHT, VID_FPS = 1280, 720, 20
 VID_FOURCC = cv.VideoWriter_fourcc(*'MJPG')
@@ -29,8 +29,8 @@ VID_OUT_FOURCC = cv.VideoWriter_fourcc(*'XVID')
 VID_OUT_BORDER_L, VID_OUT_BORDER_T, VID_OUT_BORDER_R, VID_OUT_BORDER_B = 182, 60, 1092, 660
 VID_OUT_WIDTH, VID_OUT_HEIGHT = VID_OUT_BORDER_R - VID_OUT_BORDER_L, VID_OUT_BORDER_B - VID_OUT_BORDER_T
 
-NUM_REPLAY_CMDS = 3
-REPLAY_START, REPLAY_KILL, REPLAY_SHUTDOWN = range(1, NUM_REPLAY_CMDS+1)  # Replay statuses start from 1
+NUM_RECORD_CMDS = 2
+RECORD_START, RECORD_KILL = range(1, NUM_RECORD_CMDS+1)  # Record statuses start from 1
 
 
 def read_stdin(queue):
@@ -54,7 +54,7 @@ def read_serial(ser, queue):
         queue.put(('serial', cmd))
 
 
-def process_throw(replay_cmd):
+def process_video(record_cmd):
     """ Save video """
     # Open video
     cap = cv.VideoCapture(VID_DEVICE_ID)
@@ -63,58 +63,59 @@ def process_throw(replay_cmd):
     cap.set(cv.CAP_PROP_FRAME_HEIGHT, VID_HEIGHT)
     cap.set(cv.CAP_PROP_FPS, VID_FPS)
 
+    recording, out = False, None
     while True:
-        cmd = replay_cmd.wait()
+        cmd = record_cmd.value
 
-        if cmd == REPLAY_KILL:
-            replay_cmd.set(0)
+        if cmd == RECORD_START:
+            if not recording:
+                fh, tmp_filename = tempfile.mkstemp(prefix='replay-', suffix='.avi')
+                print(f'\rSaving to {tmp_filename}\n{STDIN_PROMPT}', end='')
+                os.close(fh)
+                out = cv.VideoWriter(tmp_filename, VID_OUT_FOURCC, VID_FPS, (VID_OUT_WIDTH, VID_OUT_HEIGHT))
+
+                recording = True
+
+        elif cmd == RECORD_KILL:
+            out.release()
+
+            record_cmd.value = 0
+            recording, out = False, None
+
+            print(f'\rRecording killed\n{STDIN_PROMPT}', end='')
+
+        # Get next frame
+        ret, frame = cap.read()
+        if not ret:
             continue
+        frame = frame[VID_OUT_BORDER_T:VID_OUT_BORDER_B, VID_OUT_BORDER_L:VID_OUT_BORDER_R]
 
-        fh, tmp_filename = tempfile.mkstemp(prefix='replay-', suffix='.avi')
-        print(f'\rSaving to {tmp_filename}\n{STDIN_PROMPT}', end='')
-        os.close(fh)
-        out = cv.VideoWriter(tmp_filename, VID_OUT_FOURCC, VID_FPS, (VID_OUT_WIDTH, VID_OUT_HEIGHT))
-
-        while True:
-            if replay_cmd.cmd == REPLAY_KILL:
-                print(f'\rKilling the recording\n{STDIN_PROMPT}', end='')
-                break
-
-            # Get next frame
-            ret, frame = cap.read()
-            if not ret:
-                continue
-            frame = frame[VID_OUT_BORDER_T:VID_OUT_BORDER_B, VID_OUT_BORDER_L:VID_OUT_BORDER_R]
-
-            # Save frame
+        # Save frame
+        if recording:
             out.write(frame)
 
-            cv.waitKey(1)
+        # Show preview
+        cv.imshow('preview', frame)
 
-        out.release()
-        replay_cmd.set(0)
+        cv.waitKey(1)
 
     cap.release()
 
 
-class ReplayCmd:
+class SafeValue:
     """ A threading.Event-like class with an integer flag """
     def __init__(self):
-        self._cond = threading.Condition(threading.Lock())
-        self.cmd = 0
+        self._lock = threading.Lock()
+        self._value = 0
 
-    def set(self, cmd):
-        with self._cond:
-            self.cmd = cmd
-            if cmd:
-                self._cond.notify_all()
+    @property
+    def value(self):
+        return self._value
 
-    def wait(self):
-        with self._cond:
-            if self.cmd:
-                return self.cmd
-            self._cond.wait()
-            return self.cmd
+    @value.setter
+    def value(self, value):
+        with self._lock:
+            self._value = value
 
 
 # Ignore SIGINT
@@ -123,12 +124,12 @@ signal.signal(signal.SIGINT, signal.SIG_IGN)
 
 def main():
     cmd_queue = queue.Queue()
-    replay_cmd = ReplayCmd()
+    record_cmd = SafeValue()
 
     with serial.Serial(SERIAL_PORT, SERIAL_BAUDRATE) as ser:
         threading.Thread(target=read_stdin, args=(cmd_queue,), daemon=True).start()
         threading.Thread(target=read_serial, args=(ser, cmd_queue), daemon=True).start()
-        threading.Thread(target=process_throw, args=(replay_cmd,), daemon=True).start()
+        threading.Thread(target=process_video, args=(record_cmd,), daemon=True).start()
 
         last_cmd = None
         while True:
@@ -136,6 +137,7 @@ def main():
             cmd = cmd_queue.get()
 
             # Yield control, so that `input` has time to run
+
             time.sleep(1e-4)
 
             ## Command from serial
@@ -147,7 +149,7 @@ def main():
             cmd = cmd[1]
 
             # Don't process the next command if we are currently recording
-            if replay_cmd.cmd == REPLAY_START and cmd is not _CMD_QUEUE_SENTINEL and (
+            if record_cmd.value == RECORD_START and cmd is not _CMD_QUEUE_SENTINEL and (
                     cmd in ['w', 's', 'a', 'd', 'f', 'l'] or cmd.startswith('r ')):
                 print(f'\nWe are currently recording; please kill the recording first\n{STDIN_PROMPT}', end='')
                 continue
@@ -164,7 +166,7 @@ def main():
                 ser.write(cmd.encode())
 
             elif cmd.startswith('r '):
-                replay_cmd.set(REPLAY_START)
+                record_cmd.value = RECORD_START
 
                 cmd += '\n'
                 last_cmd = cmd.encode()
@@ -177,17 +179,16 @@ def main():
 
                 print(f'\rRunning last command: "{last_cmd[:-1].decode()}"\n{STDIN_PROMPT}', end='')
                 if last_cmd.startswith(b'r '):
-                    replay_cmd.set(REPLAY_START)
+                    record_cmd.value = RECORD_START
                 ser.write(last_cmd)
 
             elif cmd == 'k':
                 # Kill recording
-                curr_cmd = replay_cmd.cmd
-                if curr_cmd != REPLAY_START:
+                if record_cmd.value != RECORD_START:
                     print(f'\rNothing to kill; we\'re not currently recording\n{STDIN_PROMPT}', end='')
                     continue
 
-                replay_cmd.set(REPLAY_KILL)
+                record_cmd.value = RECORD_KILL
 
             elif cmd == 'q':
                 print('Bye')
